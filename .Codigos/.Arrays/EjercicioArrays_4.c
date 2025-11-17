87b2da6b8974e992d100c5082b52baddfaae0cc3
Negrito_Hacker <bfosatti@etrr.edu.ar>
2025-11-17 09:58:02 -0300

    Update EjercicioArrays_4.c

--- .Codigos/.Arrays/EjercicioArrays_4.c
+++ .Codigos/.Arrays/EjercicioArrays_4.c
@@ -1,73 +1,58 @@
 #include <stdio.h>
-#include<time.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define UNO 1
 #define TAMVEC 100
+#define MAXmain 40
+#define MINmain 10
 
 int vector[TAMVEC];
-int contarMayor();
+int nMax, nMin;
 
-int i;
-int a;
-
-int nMax=0;
-int nRepMax=0;
-
-int nMin=20;
-int nRepMin=0;
+int encontrarMaximo();
+int encontrarMinimo();
+void mostrarPosicionesMax(int maximo);
 
 int main() {
-	srand(time(NULL));
+    srand(time(NULL));
+    printf("\n--------------Bienvenido a mi programa--------------\n");
+    printf("\nGenerando 100 valores aleatorios entre 10 y 40...\n\n");
 
-	printf("\n--------------Bienvenido a mi programa--------------\n");
-	printf("\n-En este codigo te mostrare la cantidad de veces que se repite el numero entero mas grande y mas chico desde el 10 al 40 entre 100 valores y con sus ubicaciones:\n\n");
-
-////////////////////////////////CONTAR 50 VECTORES/////////////
-	for(i=0; i<TAMVEC; i++) {
-		vector[i] = rand()%(0+31)+10;
-	}
-	for(i=0; i<TAMVEC; i++) {
-		printf("Vector [%d] = %d\n",i+UNO,vector[i]);
-
-///////////////////////////NUMERO MAS GRANDE DE LOS 50///////////////////
-		if (vector[i]>nMax) {
-			nMax=vector[i];
-		}
-
-		if (vector[i]<nMin) {
-			nMin=vector[i];
-		}
-	}
-////////////////////////////CUANTO SE REPITE////////////
-	for(i=0; i<TAMVEC; i++) {
-		if (vector[i]==nMin) {
-			nRepMin=nRepMin+UNO;
-		}
-
-	}
-
-	for(i=0; i<TAMVEC; i++) {
-		if (vector[i]==nMax) {
-			nRepMax=nRepMax+UNO;
-		}
-
-	}
-///////////////////Sus maximos y donde estan/////////////
-	printf("\n-El maximo es %d y se repite %d veces en los vectores:\n",nMax,nRepMax);
-	for(i=0; i<TAMVEC; i++) {
-		if (vector[i]==nMax) {
-			printf("Vector [%d] = %d\n",i,nMax);
-		}
-	}
-
-/////////////////Sus Minimos y donde estan//////////
-	printf("\n-El minimo es %d y se repite %d veces en los vectores:\n",nMin,nRepMin);
-	for(i=0; i<TAMVEC; i++) {
-		if (vector[i]==nMin) {
-			printf("Vector [%d] = %d\n",i,nMin);
-		}
-	}
-	return 0;
-
+    for (int i = 0; i < TAMVEC; i++) {
+        vector[i] = rand() % (MAXmain - MINmain + UNO) + MINmain;
+        printf("Vector[%d] = %d\n", i + UNO, vector[i]);
+    }
+     nMax = encontrarMaximo();
+    nMin = encontrarMinimo();
+    printf("\nEl máximo es: %d\n", nMax);
+    printf("El mínimo es: %d\n", nMin);
+    printf("\nPosiciones donde aparece el máximo:\n");
+    mostrarPosicionesMax(nMax);
+    return 0;
+}
+int encontrarMaximo() {
+    int max = vector[0];
+    for (int i = UNO; i < TAMVEC; i++) {
+        if (vector[i] > max) {
+            max = vector[i];
+        }
+    }
+    return max;
+}
+int encontrarMinimo() {
+    int min = vector[0];
+    for (int i = UNO; i < TAMVEC; i++) {
+        if (vector[i] < min) {
+            min = vector[i];
+        }
+    }
+    return min;
+}
+void mostrarPosicionesMax(int maximo) {
+    for (int i = 0; i < TAMVEC; i++) {
+        if (vector[i] == maximo) {
+            printf("Vector[%d] = %d\n", i + UNO, maximo);
+        }
+    }
 }
