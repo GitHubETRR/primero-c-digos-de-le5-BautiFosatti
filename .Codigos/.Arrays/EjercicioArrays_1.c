85e089ea94c3fac292f01853982644ade977d904
Negrito_Hacker <bfosatti@etrr.edu.ar>
2025-11-17 09:45:44 -0300

    Update EjercicioArrays_1.c

--- .Codigos/.Arrays/EjercicioArrays_1.c
+++ .Codigos/.Arrays/EjercicioArrays_1.c
@@ -2,17 +2,26 @@
 #define UNO 1
 #define TAMVEC 7
 
-int vector[TAMVEC];
-int resultado=0;
+int sumaVector(int vec[], int tam);
 
 int main() {
+	int vector[TAMVEC];
+	int resultado = 0;
 	printf("--------------Bienvenido a mi programa--------------");
 	printf("\nNecesito que me escribas 7 numeros enteros para decirte su sumatoria: \n");
-	for(int i=0; i<TAMVEC; i++) {
-		printf("\n-Numero: %d\n",i+UNO);
-		scanf("%d",&vector[i]);
-		resultado=resultado+vector[i];
+	for(int i = 0; i < TAMVEC; i++) {
+		printf("\n-Numero: %d\n", i + UNO);
+		scanf("%d", &vector[i]);
 	}
-	printf("\nEl resultado de su sumatoria, es igual a: %d",resultado);
+	resultado = sumaVector(vector, TAMVEC);
+	printf("\nEl resultado de su sumatoria es: %d", resultado);
 	return 0;
+}
+
+int sumaVector(int vec[], int tam) {
+	int suma = 0;
+	for(int i = 0; i < tam; i++) {
+		suma += vec[i];
+	}
+	return suma;
 }
