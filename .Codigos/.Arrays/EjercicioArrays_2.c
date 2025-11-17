ba05d4f1c5fabf08ddafe796bc240c0d4bbd8a66
Negrito_Hacker <bfosatti@etrr.edu.ar>
2025-11-17 09:49:37 -0300

    Update EjercicioArrays_2.c

--- .Codigos/.Arrays/EjercicioArrays_2.c
+++ .Codigos/.Arrays/EjercicioArrays_2.c
@@ -2,18 +2,31 @@
 #define UNO 1
 #define TAMVEC 4
 
-int vector[TAMVEC];
-float resultado=0;
+int sumaVector(int vec[], int tam);
+float promedioVector(int vec[], int tam);
 
 int main() {
-	printf("--------------Bienvenido a mi programa--------------");
-	printf("\nNecesito que me escribas 7 numeros enteros para decirte su promedio: \n");
-	for(int i=0; i<TAMVEC; i++) {
-		printf("\n-Numero: %d\n",i+UNO);
-		scanf("%d",&vector[i]);
-		resultado=resultado+vector[i];
-	}
-	float promedio=resultado/TAMVEC;
-	printf("\nEl promedio, es igual a: %.3f",promedio);
-	return 0;
+    int vector[TAMVEC];
+    printf("--------------Bienvenido a mi programa--------------");
+    printf("\nNecesito que me escribas 4 numeros enteros para decirte su promedio: \n");
+
+    for(int i = 0; i < TAMVEC; i++) {
+        printf("\n-Numero: %d\n", i + UNO);
+        scanf("%d", &vector[i]);
+    }
+    float promedio = promedioVector(vector, TAMVEC);
+    printf("\nEl promedio es: %.3f", promedio);
+    return 0;
+}
+
+int sumaVector(int vec[], int tam) {
+    int suma = 0;
+    for(int i = 0; i < tam; i++) {
+        suma += vec[i];
+    }
+    return suma;
+}
+float promedioVector(int vec[], int tam) {
+    int suma = sumaVector(vec, tam);
+    return (float)suma / tam;
 }
