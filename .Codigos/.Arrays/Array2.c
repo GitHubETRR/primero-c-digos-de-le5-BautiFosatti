4494516fc14bdf98583cec4683d2a0715ae85632
Negrito_Hacker <bfosatti@etrr.edu.ar>
2025-11-17 09:37:49 -0300

    Add files via upload

---
+++ .Codigos/.Arrays/Array2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define UNO 1
+#define TAMVEC 4
+
+
+int sumaVector(int vec[], int tam);
+float promedioVector(int vec[], int tam);
+
+int main() {
+    int vector[TAMVEC];
+
+    printf("--------------Bienvenido a mi programa--------------");
+    printf("\nNecesito que me escribas 4 numeros enteros para decirte su promedio: \n");
+
+    for(int i = 0; i < TAMVEC; i++) {
+        printf("\n-Numero: %d\n", i + UNO);
+        scanf("%d", &vector[i]);
+    }
+
+    float promedio = promedioVector(vector, TAMVEC);
+
+    printf("\nEl promedio es: %.3f", promedio);
+
+    return 0;
+}
+
+
+int sumaVector(int vec[], int tam) {
+    int suma = 0;
+    for(int i = 0; i < tam; i++) {
+        suma += vec[i];
+    }
+    return suma;
+}
+
+
+float promedioVector(int vec[], int tam) {
+    int suma = sumaVector(vec, tam);
+    return (float)suma / tam;
+}
---
+++ .Codigos/.Arrays/Array5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define UNO 1
+#define TAMVEC 5
+
+int vectorA[TAMVEC];
+int vectorB[TAMVEC];
+
+int productoEscalar();
+
+int main() {
+	srand(time(NULL));
+
+	printf("\n--------------Bienvenido a mi programa--------------\n");
+	printf("\n-En este código multiplicaré dos vectores A y B de forma escalar:\n\n");
+
+
+	printf("-Componentes del Vector A:\n");
+	for (int i = 0; i < TAMVEC; i++) {
+		vectorA[i] = rand() % (30 + UNO) + UNO;
+		printf("A%d = %d\n", i + UNO, vectorA[i]);
+	}
+
+
+	printf("\n-Componentes del Vector B:\n");
+	for (int i = 0; i < TAMVEC; i++) {
+		vectorB[i] = rand() % (30 + UNO) + UNO;
+		printf("B%d = %d\n", i + UNO, vectorB[i]);
+	}
+
+
+	int resultado = productoEscalar();
+
+	printf("\n--El resultado del producto escalar entre los vectores A y B es igual a %d--\n", resultado);
+
+	return 0;
+}
+
+
+int productoEscalar() {
+	int total = 0;
+
+	for (int i = 0; i < TAMVEC; i++) {
+		total += (vectorA[i] * vectorB[i]);
+	}
+
+	return total;
+}
