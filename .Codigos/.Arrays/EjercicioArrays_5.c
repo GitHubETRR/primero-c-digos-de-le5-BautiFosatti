21e85d75b48d1df2f0c71be4110b6b94ee8a1df2
Negrito_Hacker <bfosatti@etrr.edu.ar>
2025-11-17 10:01:57 -0300

    Update EjercicioArrays_5.c

--- .Codigos/.Arrays/EjercicioArrays_5.c
+++ .Codigos/.Arrays/EjercicioArrays_5.c
@@ -1,42 +1,43 @@
 #include <stdio.h>
-#include<time.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define UNO 1
 #define TAMVEC 5
 
 int vectorA[TAMVEC];
 int vectorB[TAMVEC];
-int resultado=0;
-int i;
+
+int productoEscalar();
 
 int main() {
 	srand(time(NULL));
-	printf("\n--------------Bienvenido a mi programa--------------\n");
-	printf("\n-En este codigo multiplicare dos vectores A y B de forma escalar:\n\n");
 
-////////////////////////////////CONTAR 5 NUMEROS PARA EL VECTOR A /////////////
+	printf("--------------Bienvenido a mi programa--------------\n");
+	printf("\n-En este código multiplicaré dos vectores A y B de forma escalar:\n\n");
+
 	printf("-Componentes del Vector A:\n");
-	for(i=0; i<TAMVEC; i++) {
-		vectorA[i] = rand()%(0+31)+UNO;
-		printf("A%d=%d\n",i+UNO,vectorA[i]);
+	for (int i = 0; i < TAMVEC; i++) {
+		vectorA[i] = rand() % (30 + UNO) + UNO;
+		printf("A%d = %d\n", i + UNO, vectorA[i]);
 	}
 
-////////////////////////////////CONTAR 5 NUMEROS PARA EL VECTOR B /////////////
 	printf("\n-Componentes del Vector B:\n");
-	for(i=0; i<TAMVEC; i++) {
-		vectorB[i] = rand()%(0+31)+UNO;
-		printf("B%d=%d\n",i+UNO,vectorB[i]);
+	for (int i = 0; i < TAMVEC; i++) {
+		vectorB[i] = rand() % (30 + UNO) + UNO;
+		printf("B%d = %d\n", i + UNO, vectorB[i]);
 	}
 
-	for(i=0; i<TAMVEC; i++) {
-		resultado=resultado+vectorB[i]*vectorA[i];
-	}
-	
-	printf("\n--El resultado del producto escalar entre los vectores A y B es igual a %d--",resultado);
+	int resultado = productoEscalar();
+	printf("\n--El resultado del producto escalar entre los vectores A y B es igual a %d--\n", resultado);
 	return 0;
-
-
 }
 
+int productoEscalar() {
+	int total = 0;
+	for (int i = 0; i < TAMVEC; i++) {
+		total += (vectorA[i] * vectorB[i]);
+	}
+	return total;
+}
 
